deb76d28b2f743fbddfe2a0a0c47b377f5d817a9
Incrementing <hello@gian.sh>
2023-08-29 11:50:36 +0100

    CreateSubscriptionPriceChange

--- asc/subscriptions.go
+++ asc/subscriptions.go
@@ -3,6 +3,7 @@ package asc
 import (
 	"context"
 	"net/url"
+	"time"
 )
 
 // SubscriptionsService handles communication with methods related to Auto-Renewable Subscriptions
@@ -111,6 +112,25 @@ type SubscriptionLocalizationResponse struct {
 	Links DocumentLinks            `json:"links"`
 }
 
+type SubscriptionPriceCreateAttributes struct {
+	PreserveCurrentPrice bool      `json:"preserveCurrentPrice"`
+	Preserved            bool      `json:"preserved"`
+	StartDate            time.Time `json:"startDate"`
+}
+
+type SubscriptionPriceCreate struct {
+	Attributes    SubscriptionPriceCreateAttributes `json:"attributes"`
+	ID            string                            `json:"id"`
+	Links         ResourceLinks                     `json:"links"`
+	Relationships any                               `json:"relationships"`
+	Type          string                            `json:"type"`
+}
+
+type SubscriptionPriceCreateResponse struct {
+	Data  SubscriptionPriceCreate `json:"data"`
+	Links DocumentLinks           `json:"links"`
+}
+
 type SubscriptionGroupLocalization struct {
 	Attributes    SubscriptionGroupLocalizationAttributes `json:"attributes"`
 	ID            string                                  `json:"id"`
@@ -158,6 +178,22 @@ type SubscriptionGroupLocalizationData struct {
 		App struct {
 			Data *RelationshipData `json:"data"`
 		} `json:"subscriptionGroup"`
+	} `json:"relationships"`
+	Type string `json:"type"`
+}
+
+// SubscriptionPriceCreateData defines model for SubscriptionPriceCreateRequest.Data
+//
+// https://developer.apple.com/documentation/appstoreconnectapi/subscriptionpricecreaterequest/data
+type SubscriptionPriceCreateData struct {
+	Attributes    SubscriptionPriceCreateAttributes `json:"attributes"`
+	Relationships struct {
+		Subscription struct {
+			Data *RelationshipData `json:"data"`
+		} `json:"subscription"`
+		SubscriptionPricePoint struct {
+			Data *RelationshipData `json:"data"`
+		} `json:"subscriptionPricePoint"`
 	} `json:"relationships"`
 	Type string `json:"type"`
 }
@@ -269,6 +305,36 @@ func (s *SubscriptionsService) CreateSubscriptionLocalization(ctx context.Contex
 			}{Data: &RelationshipData{ID: subscriptionID, Type: "subscriptions"}},
 		},
 		Type: "subscriptionLocalizations",
+	}), res)
+	return res, resp, err
+}
+
+// CreateSubscriptionPriceChange schedules a subscription price change for a specific territory.
+//
+// https://developer.apple.com/documentation/appstoreconnectapi/create_a_subscription_price_change
+func (s *SubscriptionsService) CreateSubscriptionPriceChange(ctx context.Context, subscriptionID, priceID string, preserveCurrentPrice bool) (*SubscriptionPriceCreateResponse, *Response, error) {
+	res := new(SubscriptionPriceCreateResponse)
+	resp, err := s.client.post(ctx, "v1/subscriptionPrices", newRequestBody(SubscriptionPriceCreateData{
+		Attributes: SubscriptionPriceCreateAttributes{
+			PreserveCurrentPrice: preserveCurrentPrice,
+			StartDate:            time.Now(),
+		},
+		Relationships: struct {
+			Subscription struct {
+				Data *RelationshipData `json:"data"`
+			} `json:"subscription"`
+			SubscriptionPricePoint struct {
+				Data *RelationshipData `json:"data"`
+			} `json:"subscriptionPricePoint"`
+		}{
+			Subscription: struct {
+				Data *RelationshipData `json:"data"`
+			}{Data: &RelationshipData{ID: subscriptionID, Type: "subscriptions"}},
+			SubscriptionPricePoint: struct {
+				Data *RelationshipData `json:"data"`
+			}{Data: &RelationshipData{ID: priceID, Type: "subscriptionPricePoints"}},
+		},
+		Type: "subscriptionPrices",
 	}), res)
 	return res, resp, err
 }
